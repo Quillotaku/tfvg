d3899968b2118d47128092b32e2d0d062d61aa02
Ionut Alexandru Petrin <petrin.ionut@evolutio.com>
2023-09-26 16:59:09 +0200

    some changes

--- tfvg.py
+++ tfvg.py
@@ -1,6 +1,83 @@
 import glob
 import re
 from jinja2 import Template
+import sys
+import os
+
+def get_template(tpl):
+    if tpl == "var_with_default":
+        var_template = '''
+{% for i in variables -%}
+variable "{{ i }}" { default = }
+
+{% endfor -%}
+'''
+    elif tpl == "var_with_description":
+        var_template = '''
+{% for i in variables -%}
+variable "{{ i }}" { description = "" }
+
+{% endfor -%}
+    '''
+    elif tpl == "var_with_description_and_default":
+        var_template = '''
+{% for i in variables -%}
+variable "{{ i }}" {
+  default = 
+  description = ""
+}
+
+{% endfor -%}
+'''
+    elif tpl == "empty":
+        var_template = '''
+    {% for i in variables -%}
+    variable "{{ i }}" {}
+
+    {% endfor -%}
+    '''
+    return var_template
+
+def help():
+    try:
+        sys.argv.index("--help")
+        print("""
+        Uso: python tfvg.py --help
+        --template TEMPLATE_OPTION: Nombre de la template de jinja para generar el fichero de variables.tf. Si no se indica coge la template por defecto.
+        --retemplate: Borra el fichero actual de variables.tf y lo vuelve a generar a partir del fichero de template que se indique o el por defecto. Se puede usar en conjunto con --template.
+
+        TEMPLATE_OPTION:
+
+           * var_with_default:
+                {% for i in variables -%}
+                variable "{{ i }}" { default = }
+
+                {% endfor -%}
+
+            * var_with_description:
+                {% for i in variables -%}
+                variable "{{ i }}" { description = "" }
+
+                {% endfor -%}
+
+            * var_with_description_and_default:
+                {% for i in variables -%}
+                variable "{{ i }}" {
+                  default = 
+                  description = ""
+                }
+
+                {% endfor -%}
+
+            * empty:
+                {% for i in variables -%}
+                variable "{{ i }}" {}
+
+                {% endfor -%}
+        """,)
+        sys.exit(1)
+    except ValueError:
+        pass
 
 def get_vars():
     variables = []
@@ -11,7 +88,7 @@ def get_vars():
             for line in tf:
                 if re.search('var\..*', line):
                     if not re.search('\${', line):
-                        variables += [line.replace(',','').strip().split('var.')[1]]
+                        variables += [line.replace(',','').strip().split('.')[1]]
                     if re.search('\${', line):
                         variables += re.findall('var\.\w+\}', line)
     for vars in variables:
@@ -37,21 +114,22 @@ def clear_vars():
     current_vars = check_vars()
     got_vars = get_vars()
     add_vars = set(got_vars) - set(current_vars)
-    return list(add_vars) 
+    return list(add_vars)
 
 def write_vars():
-    var_template = '''
-{% for i in variables -%}
-variable "{{ i }}" {
-    type = 
-    description = ""
-    default = ""
-}
-
-{% endfor -%}
-'''
-    with open(file='variables.tf', encoding='UTF-8', mode='a') as var_file:
-        var_file.write(Template(var_template).render(variables=clear_vars()))
+    try:
+        var_template = get_template(sys.argv[sys.argv.index("--template") + 1])
+    except:
+        var_template = get_template("empty")
+    # print(var_template)
+    try:
+        sys.argv.index("--retemplate")
+        os.remove("variables.tf")
+        with open(file='variables.tf', encoding='UTF-8', mode='a') as var_file:
+            var_file.write(Template(var_template).render(variables=clear_vars()))
+    except:
+        with open(file='variables.tf', encoding='UTF-8', mode='a') as var_file:
+            var_file.write(Template(var_template).render(variables=clear_vars()))
 
 def pretty_output():
     lines = []
@@ -64,6 +142,8 @@ def pretty_output():
             var_file_w.write(line)
 
 
+
+help()
 check_var_file()
 check_vars()
 clear_vars()
